bf6a954d0d0a98bfaf45dd5403bb8e87b904941e
Pumudi Jayasinghe <pumudij@zone24x7.lk>
2020-10-22 20:12:15 +0530

    props and components

--- blog-post/src/index.js
+++ blog-post/src/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SingleComment from './SingleComment';
+import UserCard from './UserCard';
+import profile1 from './images/image1.jpg';
+import profile2 from './images/image2.jpg';
+import profile3 from './images/image3.jpg';
 
 const App = () => {
     return(
-        <div>
-            Hello World
+        <div className='ui comments'>
+            <UserCard>
+                <div>Hello im Alex from Mexico</div>
+            </UserCard>
+            <UserCard>
+            <SingleComment 
+                name='Sarah' 
+                date='Yesterday 5:00PM'
+                text='It is amazing'
+                profile={profile1}/>
+            </UserCard>
+            <UserCard>
+            <SingleComment 
+                name='Jack' 
+                date='Today 3:00PM'
+                text='Great job!!'
+                profile={profile2}/>
+            </UserCard>
+            <UserCard>
+            <SingleComment 
+                name='Mike' 
+                date='Today 4:00PM'
+                text='Thanks....'
+                profile={profile3}/>
+            </UserCard>
         </div>
     )
 }
