b03b5679c448fe50624185172e70b7e9ddc10d46
Pumudi Jayasinghe <pumudij@zone24x7.lk>
2020-10-23 23:55:21 +0530

    binding method 1

--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -2,23 +2,27 @@ import React from 'react';
 
 class SearchInput extends React.Component {
 
+    constructor(){
+        super();
+        this.onFormSubmit =this.onFormSubmit.bind(this)
+    }
     state = {
         entry:''
     }
-    onInputChange(event){
-        console.log(event.target.value)
+    onFormSubmit( event) {
+        event.preventDefault()
+        console.log(this.state.entry)
     }
     render(){
         return(
             <div className='ui segment'>
-                <form className='ui form' action="">
+                <form onSubmit={this.onFormSubmit} className='ui form' action="">
                     <div className='field'>
                         <div className='ui massive icon input'>
                             <input 
                                 type="text" 
                                 placeholder='search...'
                                 onChange={(event) => this.setState({entry: event.target.value})}
-                                /*onChange={this.onInputChange}*/
                                 value={this.state.entry}
                                 />                            
                             <i className='search icon'></i>
