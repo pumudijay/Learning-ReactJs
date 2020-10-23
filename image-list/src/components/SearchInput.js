cad568b764d5ff0945814f80d2cf237f5204fd79
Pumudi Jayasinghe <pumudij@zone24x7.lk>
2020-10-23 23:14:41 +0530

    event handlers

---
+++ image-list/src/components/SearchInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class SearchInput extends React.Component {
+
+    onInputChange(event){
+        console.log(event.target.value)
+    }
+    render(){
+        return(
+            <div className='ui segment'>
+                <form className='ui form' action="">
+                    <div className='field'>
+                        <div className='ui massive icon input'>
+                            <input 
+                                type="text" 
+                                placeholder='search...'
+                                onChange={this.onInputChange}>
+                            </input>
+                            <i className='search icon'></i>
+                        </div>
+                    </div>                    
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
