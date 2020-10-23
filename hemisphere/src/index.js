d87268057dd84fb245ed943d010cf3c4f3dfee58
Pumudi Jayasinghe <pumudij@zone24x7.lk>
2020-10-23 12:25:58 +0530

    state

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -13,14 +13,28 @@ import ReactDOM from 'react-dom';
 */
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { latitude: null, errorMessage:'' }
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
+      (position) => {
+        this.setState( { latitude: position.coords.latitude })
+      },
+      (error) => {this.setState({ errorMessage: error.message }) }
     )
-    return(
-      <div>Hello World</div>
-    )
+  }
+  render() {
+    
+    if( this.state.errorMessage && !this.state.latitude){
+      return( <div> {this.state.errorMessage} </div> )
+    }
+    if( !this.state.errorMessage && this.state.latitude){
+      return( <div> {this.state.latitude} </div> )
+    }
+    else{
+      return( <div> Loading.... </div> )
+    }
   }
 }
 
