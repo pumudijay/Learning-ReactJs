b929faa8e4d8e92cbd85637b20423b6fb926abcd
Pumudi Jayasinghe <pumudij@zone24x7.lk>
2020-10-27 21:02:50 +0530

    async -error

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import SearchInput from './SearchInput';
+import ImageList from './ImageList';
 
-const App = () => {
+class App extends React.Component {
 
-    return(
-        <div className='ui container' style={{marginTop:'30px'}}>
-            <SearchInput/>
-        </div>
-    )
+    state = { image:[] }
+    onSearchSubmit = async (entry) => {
+        const response = await axios.get('https://pixabay.com/api/?key=18876945-1710576cf72ef274accd4062d&q='+ {entry} + '&image_type=photo');
+        this.setState({ images: response.data.hits })
+    }
+    render(){
+        return(
+            <div className='ui container' style={{marginTop:'30px'}}>
+                <SearchInput onSearchSubmit={ this.onSearchSubmit } />
+                <ImageList images={this.state.images} />
+            </div>
+            
+            
+        )
+    }
+    
 }
 
 export default App;
--- image-list/src/components/SearchInput.js
+++ image-list/src/components/SearchInput.js
@@ -7,12 +7,12 @@ class SearchInput extends React.Component {
         /*Arrow function bind this to its function*/
     onFormSubmit = ( event) => {
         event.preventDefault()
-        console.log(this.state.entry)
+        this.props.onSearchSubmit(this.state.entry)
     }
     render(){
         return(
             <div className='ui segment'>
-                <form onSubmit={this.onFormSubmit} className='ui form' action="">
+                <form onSubmit={this.onFormSubmit} className='ui form' >
                     <div className='field'>
                         <div className='ui massive icon input'>
                             <input 
