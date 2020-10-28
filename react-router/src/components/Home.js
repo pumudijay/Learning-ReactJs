52f1f47765f62290e024ec3d908aa89d0988d77d
Pumudi Jayasinghe <pumudij@zone24x7.lk>
2020-10-29 00:58:19 +0530

    routing

---
+++ react-router/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className='ui raised very padded text container segment' style={{marginTop:'80px'}}>
+            <h3 className='ui header'>
+                Home
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacus non elit auctor dignissim. Nunc laoreet vehicula est lacinia lobortis. Nullam placerat condimentum felis. Etiam eu diam mi. Nam ac turpis auctor, porttitor est a, mollis diam. Proin luctus nunc in arcu accumsan, nec varius quam pharetra. Aenean vel justo vel eros consectetur commodo eget quis urna. Sed tempus sem vitae urna congue, a pellentesque sapien faucibus. Donec vitae ligula in diam bibendum vestibulum tempor nec turpis. Morbi vestibulum tempor mauris, non varius erat dignissim ac. Morbi dapibus diam non risus ullamcorper, ac fringilla magna accumsan. Maecenas ornare dapibus mauris, eu congue justo sollicitudin eu. Suspendisse laoreet mauris non facilisis pulvinar. Fusce vulputate auctor felis in imperdiet.
+            </p>
+        </div>
+    )
+}
+
+export default Home;
