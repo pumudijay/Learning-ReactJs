2e845aca9b71ed7236a95b7455594be668527d92
pumudijay <jayasinghepumudi08@gmail.com>
2020-11-04 23:19:30 +0530

    context-consumer

--- contextapi/src/components/TodoList.js
+++ contextapi/src/components/TodoList.js
@@ -5,15 +5,20 @@ class TodoList extends React.Component {
     static contextType = ThemeContext;
 
     render(){
-        const { isDarkTheme, darkTheme, lightTheme } = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-
         return(
-            <div style={{background:theme.background, color:theme.text, height:'140px', textAlign:'center'}}>
-                <p className='item'>Plan a family trip</p>
-                <p className='item'>Go for shopping for dinner</p>
-                <p className='item'>Go for a walk</p>
-            </div>
+            <ThemeContext.Consumer>{( context ) => {
+                const { isDarkTheme, darkTheme, lightTheme } = context;
+                const theme = isDarkTheme ? darkTheme : lightTheme;
+                return(
+                    <div style={{background:theme.background, color:theme.text, height:'140px', textAlign:'center'}}>
+                        <p className='item'>Plan a family trip</p>
+                        <p className='item'>Go for shopping for dinner</p>
+                        <p className='item'>Go for a walk</p>
+                    </div>
+                )
+            }}
+
+            </ThemeContext.Consumer>
         )
     }
 }
