8f90aae1645939831e2791d44f30d9b3de2b78da
pumudijay <jayasinghepumudi08@gmail.com>
2020-11-05 18:22:34 +0530

    useContext and useHooks

--- contextapi/src/components/TodoList.js
+++ contextapi/src/components/TodoList.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 import AuthContextProvider, { AuthContext } from '../context/AuthContext';
+import { TodoListContext } from '../context/TodoListContext';
 
-class TodoList extends React.Component {
+/*class TodoList extends React.Component {
     static contextType = ThemeContext;
 
     render(){
@@ -36,5 +37,50 @@ class TodoList extends React.Component {
             
         )
     }
-}
+}*/
+
+const TodoList = () => {
+    const [todo, setTodo] = useState('');
+    const { todos, addTodo, removeTodo } = useContext(TodoListContext);
+    const { isDarkTheme, darkTheme, lightTheme, changeTheme } = useContext(ThemeContext);
+    const { isLoggedIn, changeAuthStatus } = useContext(AuthContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+
+    const handleChange = (e) => {
+        setTodo(e.target.value)
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        addTodo(todo);
+    }
+
+    const handleRemoveTodo = (e) => {
+        const id = e.target.id;
+        removeTodo(id)
+    }
+
+    return(
+        <div style={{background:theme.background, color:theme.text, textAlign:'center'}}>
+            <h4 onClick={changeAuthStatus} style={{textAlign:'end'}}>
+                { isLoggedIn ? 'logged in' : 'logged out' }
+            </h4>
+            {
+                todos.length ? (
+                    todos.map((todo) => {
+                        return <p id={todo.id} key={todo.id} onClick={handleRemoveTodo} className='item'>{todo.text}</p>
+                    })
+                ) : (
+                    <div>You have no todo</div>
+                )
+            }
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor='todo'>Add todo: </label>
+                <input type='text' id='todo' onChange={handleChange}></input>
+                <input type='submit' value='ADD TODO' className='ui button primary'></input>
+            </form>
+            <button className='ui button primary' onClick={changeTheme}>change the theme</button>
+        </div>
+    );
+};
 export default TodoList;
--- contextapi/src/context/AuthContext.js
+++ contextapi/src/context/AuthContext.js
@@ -1,22 +1,21 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const AuthContext = createContext();
 
-class AuthContextProvider extends React.Component {
+const AuthContextProvider = ( {children} ) => {
 
-    state={
-        isLoggedIn: false
-    };
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
 
-    changeAuthStatus = () => {
-        this.setState({ isLoggedIn: !this.state.isLoggedIn});
+    const changeAuthStatus = () => {
+        setIsLoggedIn(!isLoggedIn);
     };
-    render(){
+    
         return(
-            <AuthContext.Provider value={{ ...this.state, changeAuthStatus: this.changeAuthStatus }}>
-                {this.props.children}
+            <AuthContext.Provider value={{isLoggedIn, changeAuthStatus }}>
+                {children}
             </AuthContext.Provider>
         )
-    }
+    
 }
 export default AuthContextProvider;
